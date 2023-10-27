63b832ea3ec23f946c04f4a70b4e71b8e5a34d9d
Henrique <henriquegil123@gmail.com>
2023-10-27 17:19:09 -0300

    Função do CF

--- linux_scripts/commands_linux.py
+++ linux_scripts/commands_linux.py
@@ -1,18 +1,20 @@
 import os
 
-def arquivo(domainname: str, ):
+def arquivo( id: str, domainname: str, arn: str, webaclid: str, originname: str, origindomain: str ):
     arq = str(f""" cat << EOF >> cloudformation.txt
 Name:
-Distribution ID:
-ARN:
-Domain Name: 
+Distribution ID: {id}
+ARN: {arn}
+Domain Name: {domainname}
 SSL Certificate:
 Log Bucket:
 Log Prefix:
 Cookie Logging:
 Price Class:
-AWS WAF
-Origins
+AWS WAF: {webaclid}
+Origins:
+    Origin Name: {originname}
+    Origin Domain: {origindomain}
 EOF""")
     os.system(arq)
 
