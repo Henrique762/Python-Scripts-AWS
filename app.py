08678d1554173bb6a0539d1bb2d67d1814150d5d
Gabriel Cecconi <gabriel.cecconi25@gmail.com>
2023-10-27 17:27:27 -0300

    Começo do script app e de loadbalancer

---
+++ app.py
@@ -0,0 +1,4 @@
+import regex
+
+print('Importe alguns valores para darmos inicio ao documento:')
+region = str(input('Qual região devemos fazer a análise: '))
---
+++ aws_resources/loadbalancer_function.py
@@ -0,0 +1,5 @@
+import boto3
+
+
+
+cf_client = boto3.client('cloudfront', region_name='us-east-1')
