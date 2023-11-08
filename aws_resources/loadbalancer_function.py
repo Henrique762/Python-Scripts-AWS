4bcc64d8cdf6e2b166da24f2562bd96d66f145e7
Gabriel Cecconi <gabriel.cecconi25@gmail.com>
2023-11-08 18:52:12 -0300

    Finalizado documentação de LoadBalancer

--- aws_resources/loadbalancer_function.py
+++ aws_resources/loadbalancer_function.py
@@ -22,16 +22,24 @@ def loadbalancer():
     for load_balancer in response['LoadBalancers']:
 
         # Extrair informações
-        name = "null"
-        project = "null"
-        environment = "null"
+        name = 'null'
+        project = 'null'
+        environment = 'null'
         lbname = load_balancer['LoadBalancerName']
+        arn = load_balancer['LoadBalancerArn']
         lbtype = load_balancer['Type']
         lbstate = load_balancer['State']['Code']
         lbscheme = load_balancer['Scheme']
+
+        target_groups = elbv2_client.describe_target_groups(LoadBalancerArn=arn)
+        tg = [tg_id['TargetGroupArn'] for tg_id in target_groups['TargetGroups']]
+
         vpc_id = load_balancer['VpcId']
         subnets = [subnet['SubnetId'] for subnet in load_balancer['AvailabilityZones']]
         availability_zone = [az['ZoneName'] for az in load_balancer['AvailabilityZones']]
+        
+        log = 'null'
+        waf = 'null'
 
         #Verificação de TAGs básicas
         tag_response = elbv2_client.describe_tags(
@@ -45,10 +53,18 @@ def loadbalancer():
                     project = tag['Value']
                 elif tag['Key'] == 'Environment':
                     environment = tag['Value']
+        
+        #Verificar a existência de logs e WAF
+        attribute_response = elbv2_client.describe_load_balancer_attributes(LoadBalancerArn=arn)
+        for attribute in attribute_response['Attributes']:
+            if attribute['Key'] == 'access_logs.s3.enabled':
+                log = attribute['Value']
+            elif attribute['Key'] == 'waf.enabled':
+                waf = attribute['Value']
 
         #Create file
         output_file = f'{lbname}_loadbalancer.txt'
-        loadbalancer_file(name, project, environment, lbname, lbtype, lbstate, lbscheme, vpc_id, subnets, availability_zone, output_file)
+        loadbalancer_file(name, project, environment, lbname, arn, lbtype, lbstate, lbscheme, vpc_id, subnets, availability_zone, tg, log, waf, output_file)
     
     return {'message': 'created file with load balancer info'}
 
--- linuxscripts/commands_linux.py
+++ linuxscripts/commands_linux.py
@@ -19,18 +19,28 @@ EOF""")
     os.system(arq)
 
 
-def loadbalancer_file(name: str, project: str, env: str, lbname: str, lbtype: str, lbstate: str, lbscheme: str, vpc_id: str, subnets: str, azs: str, output_file: str):
+def loadbalancer_file(name: str, project: str, env: str, lbname: str, arn: str, lbtype: str, lbstate: str, lbscheme: str, vpc_id: str, subnets: str, azs: str, tg: str, log: str, waf:str, output_file: str):
     #Verificação da existência do arquivo
     if os.path.exists(output_file):
         with open(output_file, "w") as file:
             file.truncate(0)  #Remove o conteúdo do arquivo
-
+            file.write(f"Load Balancer File")
         print(f"Conteúdo do arquivo {output_file} foi removido.")
+
     else:
         with open(output_file, "w") as file:
             file.write(f"Load Balancer File")
-
         print(f"O arquivo {output_file} foi criado com conteúdo inicial.")
+
+    # Converter as variáveis que podem ser listas em formato de lista
+    subnets = subnets if isinstance(subnets, list) else [subnets]
+    azs = azs if isinstance(azs, list) else [azs]
+    tg = tg if isinstance(tg, list) else [tg]
+
+    #Formatar as listas com "-"
+    subnets_str = "\n- ".join(subnets)
+    azs_str = "\n- ".join(azs)
+    tg_str = "\n- ".join(tg)
 
     with open(output_file, "w") as file:
         file.write(f"""
@@ -42,8 +52,19 @@ Type: {lbtype}
 State: {lbstate}
 Scheme: {lbscheme}
 
+Target Groups
+TG:
+- {tg_str}
+
 Network
 VpcId: {vpc_id}
-Subnets: {subnets}
-Availability Zone: {azs}""")
+Subnets:
+- {subnets_str}
+Availability Zone:
+- {azs_str}
+
+Atributos
+Logs: {log}
+WAF: {waf}
+""")
     return {'message': f'file {output_file} created'}
